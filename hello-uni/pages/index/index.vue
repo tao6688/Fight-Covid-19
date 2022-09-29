3724ead6cc6e85aa0556d255ec9a0cb008bd8318
LSX <lsx374686571@163.com>
2022-09-29 16:14:49 +0800

    导入echarts组件，生成图表示例

--- hello-uni/pages/index/index.vue
+++ hello-uni/pages/index/index.vue
@@ -4,15 +4,20 @@
 		<view class="intro">本项目已包含uni ui组件，无需import和注册，可直接使用。在代码区键入字母u，即可通过代码助手列出所有可用组件。光标置于组件名称处按F1，即可查看组件文档。</view>
 		<text class="intro">详见：</text>
 		<uni-link :href="href" :text="href"></uni-link>
+		<GraphTest></GraphTest>
 	</view>
 </template>
 
 <script>
+	import GraphTest from '@/components/graph/graph_test.vue'
 	export default {
 		data() {
 			return {
 				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
 			}
+		},
+		components:{
+			GraphTest
 		},
 		methods: {
 
