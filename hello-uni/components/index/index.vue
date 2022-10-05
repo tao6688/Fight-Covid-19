c622e6b3a63288ec74fa511d6086fe95863ecf19
jh7567h <64450121+jh7567h@users.noreply.github.com>
2022-10-06 00:18:35 +0800

    更新商店

---
+++ hello-uni/components/index/index.vue
@@ -0,0 +1,41 @@
+<template>
+	<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+		<swiper-item>
+			<view class="swiper-item">
+				<image class='swiper-img' src="../../static/imgs/Storeimg/cd6f7f72a133945849c8c26f6a61770529df583b8e6cb-oS5AvC.png"></image>
+			</view>
+		</swiper-item>
+		<swiper-item>
+			<view class="swiper-item">
+				<image class='swiper-img' src="../../static/imgs/Storeimg/47dbc0a25083672aeabdea040bd13cabc6cd4c8121450-olXTkV_fw1200.png"></image>
+			</view>
+		</swiper-item>
+		<swiper-item>
+			<view class="swiper-item">
+				<image class='swiper-img' src="../../static/imgs/Storeimg/6a42334dad9485140262b01f2ce42bef.png"></image>
+			</view>
+		</swiper-item>
+	</swiper>
+</template>
+
+<script>
+	export default {
+		name:"index",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style scoped>
+swiper{
+	width: 100%;
+	height: 300rpx;
+}
+.swiper-img{
+	width: 100%;
+	height: 300rpx;
+}
+</style>
--- hello-uni/pages/store/store.vue
+++ hello-uni/pages/store/store.vue
@@ -1,15 +1,29 @@
 <template>
-	<view>
-		商城
+	<view class='store'>
+		<index></index>
+		<view class='items'>
+			<view class='items-small'>
+				<image class='items-img' src="../../static/imgs/Storeimg/shangyi.jpg"></image>
+				<image class='items-img' src="../../static/imgs/Storeimg/kuzi.jpg"></image>
+				<image class='items-img' src="../../static/imgs/Storeimg/xiezi.png"></image>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	import index from '@/components/index/index.vue'
 	export default {
 		data() {
 			return {
 				
 			}
+		},
+		components:{
+			index
+		},
+		onLoad() {
+			
 		},
 		methods: {
 			
@@ -17,6 +31,17 @@
 	}
 </script>
 
-<style>
-
+<style scoped>
+.items-img{
+	padding: 20rpx;
+	display: flex;
+	flex-direction: 20rpx;
+	border: 2rpx solid #CCCCCC; 
+	overflow: hidden;
+	margin: 20rpx 0; 
+}
+.items-small{
+	width: 100%;
+	height: 240rpx;
+}
 </style>
